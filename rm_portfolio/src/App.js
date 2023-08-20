463f7c75c1a385b8a16d93a810a1c9f032b65764
ryanmadamba <38015935+ryanmadamba@users.noreply.github.com>
2023-08-20 14:27:46 -0700

    Fixing Routing

--- rm_portfolio/src/App.js
+++ rm_portfolio/src/App.js
@@ -2,18 +2,17 @@ import "./App.css";
 import { NavBar } from "./components";
 import Start from "./components/start/Start";
 import { First } from "./containers";
-import { Route, Routes } from "react-router-dom";
+import { Route, Router, Routes } from "react-router-dom";
 
 function App() {
     return (
         <div className="App">
             <div className="header">{/*<NavBar />*/}</div>
             <div className="body">
-                <Start />
+                <Routes>
+                    <Route exact path="/" element={<Start />} />
+                </Routes>
             </div>
-            <Routes>
-                <Route exact path="/" element={<Home />} />
-            </Routes>
         </div>
     );
 }
