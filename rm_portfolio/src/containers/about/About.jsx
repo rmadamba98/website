529757a4abb0a359ead77db1471b5c6f8c8f6636
ryanmadamba <38015935+ryanmadamba@users.noreply.github.com>
2023-08-21 13:44:09 -0700

    Update About Me

--- rm_portfolio/src/containers/about/About.jsx
+++ rm_portfolio/src/containers/about/About.jsx
@@ -16,7 +16,13 @@ const About = () => {
                 photo=<img src={RM1} alt="RM1"></img>
                 photo2=<img src={grad} alt="grad"></img>
                 photo3=<img src={paris} alt="paris"></img>
-                desc="I am a Software Engineer based in Cerritos, CA. I love working in spaces where algorithmic thinking meets creativity."
+                desc="I am a Software Engineer based in Cerritos, CA. I love working in spaces where algorithmic thinking meets creativity.
+                    With a background rooted in Software Engineering and a minor in Computing for Interactive Arts, I've honed my skills in 
+                    front-end development and am thrilled to be pursuing opportunities in this dynamic field. In every project I undertake, 
+                    I bring a blend of analytical thinking and artistic sensibility. I'm meticulous about writing clean and maintainable code, 
+                    ensuring that not only does the end product look fantastic, but it's also optimized for performance and scalability. 
+                    With a strong foundation in HTML, CSS, and JavaScript, I thrive on learning and adapting to new technologies and frameworks 
+                    that keep the front-end landscape ever-evolving."
                 skills="HOBBIES: Bouldering, Hiking, Music Festivals, Trying New Food"
                 link1="Resume"
                 url1="https://tinyurl.com/3n7uhvp7"
