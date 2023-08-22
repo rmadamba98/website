c389714abd2170c8d3e49ff9a015a518b733bdae
ryanmadamba <38015935+ryanmadamba@users.noreply.github.com>
2023-08-22 03:42:03 -0700

    Fixed fonts and margins...Thinking about changing colors...

---
+++ rm_portfolio/src/containers/maui/Maui.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./maui.css";
+import { Project } from "../../components";
+
+const CoRA = () => {
+    return (
+        <div className="rm__cora">
+            <Project
+                text="Check out some of Ryan's Product Design"
+                title="Mobile App Design"
+                subtitle="Covid Resource Allocator (CoRA)"
+                desc=""
+            />
+        </div>
+    );
+};
+
+export default CoRA;
